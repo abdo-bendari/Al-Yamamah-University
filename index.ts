80424c9a6f9a3ab506e6613658dc899e05f119db
bendari <abdobeendari@gmail.com>
2025-03-13 19:29:44 +0200

    VPS cm

--- index.ts
+++ index.ts
@@ -15,8 +15,9 @@ app.use(morgan("dev"));
 app.use(helmet());
 bootstrap(app);
 const port = process.env.PORT || 5000;
+const host = process.env.HOST || "0.0.0.0";
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
 app.get("/test", (req, res) => {
   res.json({ message: "API is working!" });
